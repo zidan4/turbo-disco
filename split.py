9a4b6c5547ac83364b386bdae6ce77d5c29bfbd2
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-04-14 18:56:14 +0300

    Create split.py

---
+++ split.py
@@ -0,0 +1,10 @@
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+housing["income_cat"] = pd.cut(housing["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
+
+plt.xlabel("Income category")
+plt.ylabel("Number of districts")
+plt.show()
